757cac18bb527b692925161f0238d8a81990cd4a
Jolene Sitienei <sitieneijolene@gmail.com>
2025-07-04 17:00:44 +0300

    add the database infomation

--- app/src/main/java/com/ics/journaler/dbHelper.java
+++ app/src/main/java/com/ics/journaler/dbHelper.java
@@ -2,5 +2,10 @@ package com.ics.journaler;
 
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class dbHelper {
+public class dbHelper extends SQLiteOpenHelper {
+
+    //database information - cover of the journal book
+    private static final String DATABASE_NAME = "Journaler.db";
+    private static final int DATABASE_VERSION = 1;
+
 }
