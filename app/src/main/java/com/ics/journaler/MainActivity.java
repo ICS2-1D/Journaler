4e902c3c65da0385972a878c4d1fe586e2326f01
jolene-jebet <sitieneijolene@gmail.com>
2025-07-07 11:12:04 +0300

    final final touches on the entire app

--- app/src/main/java/com/ics/journaler/MainActivity.java
+++ app/src/main/java/com/ics/journaler/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        // Optional: adjust for system bars
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
